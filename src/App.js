8c9c054c2a8196b4d1bd4274554b8a11c9ada1aa
Oren Levi <oren.stu@gmail.com>
2020-11-24 12:07:32 +0200

    Use query results with react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useQuery, gql } from '@apollo/client'
+
+const exchangeRatesQuery = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`
 
 function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+  const { loading, error, data } = useQuery(exchangeRatesQuery)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
     </div>
-  )
+  ))
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,29 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
   cache: new InMemoryCache()
 })
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  }).then(result => {
-    console.log(result)
-  })
-
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 )
